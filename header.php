6f9088a680dfe668308b73306f1e0f78a42fc55a
Tomasz Bajorek <tbajorek3@gmail.com>
2020-09-15 01:23:17 +0200

    remove default css script

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
     <meta property="og:type" content="article">
     <meta property="article:content_tier" content="free">
     <?php if (is_single() && has_post_thumbnail()) : ?>
