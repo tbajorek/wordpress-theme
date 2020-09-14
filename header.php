af18c52336bb3a58b3091a3fa31cff33c05719dc
Tomasz Bajorek <tbajorek3@gmail.com>
2020-09-15 00:52:51 +0200

    fix header

--- header.php
+++ header.php
@@ -6,9 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+    <meta property="og:type" content="article">
+    <meta property="article:content_tier" content="free">
     <?php if (is_single() && has_post_thumbnail()) : ?>
     <meta property="og:image" content="<?php echo esc_url(get_the_post_thumbnail_url()); ?>"/>
     <meta name="twitter:image" content="<?php echo esc_url(get_the_post_thumbnail_url(null, 'twitter-thumbnail')); ?>">
+    <?php else: ?>
+    <meta property="og:image" content="<?php bloginfo('template_url'); ?>/images/default_image.jpg"/>
+    <meta name="twitter:image" content="<?php bloginfo('template_url'); ?>/images/default_image.jpg">
     <?php endif; ?>
     <meta property="og:title" content="<?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?>" />
     <meta name="twitter:title" content="<?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?>">
